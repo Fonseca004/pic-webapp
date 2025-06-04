815f0e10e366910818539cd9617b25794ef86b0f
José Fonseca <jose.felix.da.fonseca@tecnico.ulisboa.pt>
2025-06-04 23:42:36 +0100

    PWA

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'SmarRackAPP',
+        short_name: 'SmartRack',
+        description: 'SmartRack WebApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#e7e5e4',
+        theme_color: '#024a70',
+        icons: [
+            {
+                src: '/home.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/home.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    }
+}
